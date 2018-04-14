7f9402554c9d41f9a985c92d6d3da366577d155e
slorenz <lorenz.stefan@gmail.com>
2018-04-14 19:47:13 +0200

    added .gitignore

--- fs/init.js
+++ fs/init.js
@@ -10,92 +10,122 @@ load('api_dht.js');
 load('api_adc.js');
 load('api_rpc.js');
 
-//Pins
-let ResetPin = 0;
-let LedPin = 16;
-let DHTpin = 22;
-let SOILpin = 32;
-let LIGHTpin = 34;
+// Pins
+let resetPin = 0;
+let statusLightPin = 16;
+let dhtPin = 22;
+let moisturePin = 32;
 
 // Turn on status led
-GPIO.set_mode(LedPin, GPIO.MODE_OUTPUT);
-GPIO.write(LedPin, 0);
+GPIO.set_mode(statusLightPin, GPIO.MODE_OUTPUT);
+GPIO.write(statusLightPin, 0);
 
-//Reset Handler
-GPIO.set_mode(ResetPin, GPIO.MODE_INPUT);
-GPIO.set_int_handler(ResetPin, GPIO.INT_EDGE_NEG, function(ResetPin) {
-  print('Pin', ResetPin, 'got interrupt');
-  GPIO.toggle(LedPin);
-  Sys.usleep(200000);
-  GPIO.toggle(LedPin);
-  Sys.usleep(200000);
-  GPIO.toggle(LedPin);
-  Sys.usleep(200000);
-  GPIO.toggle(LedPin);
-  Sys.usleep(200000);
-  GPIO.toggle(LedPin);
-  Sys.usleep(200000);
-  GPIO.toggle(LedPin);
-  Sys.usleep(200000);
-  GPIO.write(LedPin, 0);
-  Cfg.set({bt:{enable:true}});
-  Cfg.set({wifi:{sta:{enable:false}}});
-  Cfg.set({wifi:{ap:{enable:false}}});
-  Cfg.set({wifi:{sta:{ssid:'',pass:''}}});
+// Reset Handler
+GPIO.set_mode(resetPin, GPIO.MODE_INPUT);
+GPIO.set_int_handler(resetPin, GPIO.INT_EDGE_NEG, function(resetPin) {
+  print('Pin', resetPin, 'got interrupt');
+  for (let i=0; i<=5; i++)
+  {
+    GPIO.toggle(statusLightPin);
+    Sys.usleep(200000);
+  }
+  GPIO.write(statusLightPin, 1);
+  
+  // enable bluetooth
+  //Cfg.set({bt:{enable:false}});
+  // disable and clear wifi-config
+  //Cfg.set({wifi:{sta:{enable:false}}});
+  //Cfg.set({wifi:{ap:{enable:false}}});
+  //Cfg.set({wifi:{sta:{ssid:'',pass:''}}});
+
   Sys.reboot(1000);
 }, null);
-GPIO.enable_int(ResetPin);
 
-ADC.enable(SOILpin);
-let dht = DHT.create(DHTpin, DHT.DHT11);
-let deviceId = Cfg.get("higrow.deviceId");
+print("Starting...");
+
+GPIO.enable_int(resetPin);
+ADC.enable(moisturePin);
+
+let dht = DHT.create(dhtPin, DHT.DHT11);
+let deviceId = Cfg.get("device.id");
+if (deviceId === "")
+{
+  deviceId = Cfg.get("higrow.deviceId");
+  Cfg.set("device.id", deviceId);
+}
+
 let connected = false;
-//let getResetReason = ffi('int getResetReason(void)');
-//let resetreason = getResetReason();
-
-// Get Reset reason
-// from user_interface.h (expressif)
-// enum rst_reason {
-//	REASON_DEFAULT_RST		= 0,	/* normal startup by power on */
-//	REASON_WDT_RST			= 1,	/* hardware watch dog reset */
-//	REASON_EXCEPTION_RST	= 2,	/* exception reset, GPIO status wont change */
-//	REASON_SOFT_WDT_RST   	= 3,	/* software watch dog reset, GPIO status wont change */
-//	REASON_SOFT_RESTART 	= 4,	/* software restart ,system_restart , GPIO status wont change */
-//	REASON_DEEP_SLEEP_AWAKE	= 5,	/* wake up from deep-sleep */
-//	REASON_EXT_SYS_RST      = 6		/* external system reset */
-// };
-
-let cloudconnect = Timer.set(1000, Timer.REPEAT, function() {
+let readSensors = Timer.set(5000, Timer.REPEAT, function() {
   let t = dht.getTemp();
   let h = dht.getHumidity();
+  let m = ADC.read(moisturePin);
+
+  print("DeviceId:",deviceId,"Temperature:",t,"Humidity:",h,"Moisture:",m);
   
-  let higrowdata = {DeviceId: deviceId, Temperature: t, Humidity: h, Water: ADC.read(SOILpin), Light: ADC.read(LIGHTpin)};
-  
-  if(deviceId!=="" && connected){
-    GPIO.write(LedPin, 1);
+  if (deviceId !== "" && connected)
+  {
+    GPIO.write(statusLightPin, 0);
+    let jsonData = {'deviceId': deviceId, 'temperature': t, 'humidity': h, 'moisture': m, timestamp: {".sv": "timestamp"}};
     HTTP.query({
-      url: 'https://api.higrow.tech/api/records',
       headers: {'Content-Type' : 'application/json'},
-      data: higrowdata,
+      url: 'https://highgrow-3e3c9.firebaseio.com/record/entry.json',  // replace with your own endpoint
+      data: jsonData,
       success: function(body, full_http_msg) 
       { 
-        print(body); 
-        ESP32.deepSleep(3600000000); //3600 seconds / 60 minutes
+        //print(body); 
+        // sleep for 15 seconds, then (re)boot up and do it all over again
+        //ESP32.deepSleep(15000000); // 15 seconds 
       },
       error: function(err) 
       { 
         print(err); 
-        ESP32.deepSleep(7200000000); //7200 seconds / 120 minutes
+        //ESP32.deepSleep(30000000); // 30 seconds
       },
     });
-    Timer.del(cloudconnect);
+
+    GPIO.write(statusLightPin, 1);
+    //Timer.del(readSensors);
+  }
+  else
+  {
+    print("DeviceId:",deviceId,"Connected:",connected);
+    GPIO.write(statusLightPin, 1);
+  }
+
+}, null);
+
+// RPC Handlers
+RPC.addHandler('HG.Temp.Read', function(args){
+  return { value: dht.getTemp() };
+});
+RPC.addHandler('HG.Humidity.Read', function(args){
+  return { value: dht.getHumidity() };
+});
+RPC.addHandler('HG.Moisture.Read', function(args){
+  return { value: ADC.read(moisturePin) };
+});
+RPC.addHandler('HG.StatusLED.Read', function(args){
+  return { value: GPIO.read(statusLightPin) };
+});
+RPC.addHandler('HG.StatusLED.On', function(args){
+  GPIO.write(statusLightPin, 0);
+  print("LED On");
+  if (GPIO.read(statusLightPin) !== 0)
+  {
+    return false;
   }
   
-  Cfg.set({higrow:{temperature:higrowdata.Temperature}});
-  Cfg.set({higrow:{water:higrowdata.Water}});
-  Cfg.set({higrow:{humidity:higrowdata.Humidity}});
-  Cfg.set({higrow:{light:higrowdata.Light}});
-},null);
+  return true;
+});
+RPC.addHandler('HG.StatusLED.Off', function(args){
+  GPIO.write(statusLightPin, 1);
+  if (GPIO.read(statusLightPin) !== 1)
+  {
+    return false;
+  }
+  
+  return true;
+});
 
 
 // Monitor network connectivity.
@@ -115,4 +145,6 @@ Event.addGroupHandler(Net.EVENT_GRP, function(ev, evdata, arg) {
     evs = 'GOT_IP';
     connected = true;
   }
+  
+  print(evs);
 }, null);
